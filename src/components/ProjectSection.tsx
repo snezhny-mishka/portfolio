73c484ea2c99b2d70598c20ea9a311e454794ba9
unknown <xopox2015@gmail.com>
2025-10-22 12:17:30 +0200

    style: add styles

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -3,12 +3,16 @@ import { faHtml5, faCss, faSquareGithub, faJs } from "@fortawesome/free-brands-s
 import { faSquareArrowUpRight } from "@fortawesome/free-solid-svg-icons";
 export const ProjectSection = () => {
     return (
-        <div className="gap-2.5 grid grid-cols-6 mx-3.75">
-            <h1 className="col-span-6 bg-blue-300 heading-section">PERSONAL PROJECTS</h1>
-            <div className="justify-self-center col-span-6 bg-green-300 border-1 rounded-xl w-fit">
-                <h1 className="px-10 py-1 font-montserrat text-sm">MEMORY GAME</h1>
+        <div className="gap-4 grid grid-cols-6 mx-3.75">
+            <h1 className="col-span-6 bg-blue-300 mb-8 heading-section">PERSONAL PROJECTS</h1>
+            <div className="relative flex justify-center col-span-6">
+                <div className="-top-8 left-1/2 absolute bg-grey rounded-t-full w-16 h-8"></div>
+                <div className="justify-self-center col-span-6 bg-green-300 border-1 rounded-xl w-fit">
+                    <h1 className="px-10 py-1 font-montserrat text-sm">MEMORY GAME</h1>
+                </div>
             </div>
-            <div className="relative col-span-6 bg-red-300">
+
+            <div className="relative col-span-6">
                 {/* <div className="relative m-auto max-w-3/4"> */}
                 <div className="absolute inset-0 bg-yellow opacity-60 m-auto rounded-xl max-w-4/5"></div>
                 <img
@@ -27,7 +31,7 @@ export const ProjectSection = () => {
                 </div>
             </div>
             <div className="relative flex justify-around gap-4 col-span-6">
-                <div className="top-2 left-20 -z-10 absolute -col-start-6 bg-yellow rounded-full w-24 h-24"></div>
+                <div className="top-2 left-1/4 -z-10 absolute -col-start-6 bg-yellow rounded-full w-18 h-18"></div>
                 <div className="px-2 py-1 border-1 rounded-xl w-fit">
                     <p className="text-sm">built with</p>
                 </div>
