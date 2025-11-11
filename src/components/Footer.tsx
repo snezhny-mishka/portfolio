72a37a73eb6a582de95bd15469cf8f8115df6c87
unknown <xopox2015@gmail.com>
2025-11-11 10:54:23 +0100

    feat: add link paths and aria-lables to anchor elements

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { faEnvelope, faCopyright } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faSquareGithub } from "@fortawesome/free-brands-svg-icons";
 
 export const Footer = () => {
-        return (
+    return (
         <footer className="bg-yellow mt-6">
             <div className="justify-items-center gap-4 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 pt-3">
                 <div className="flex justify-center items-center gap-2">
@@ -12,8 +12,21 @@ export const Footer = () => {
                 </div>
                 <div className="flex justify-center items-center gap-2">
                     <p className="">Socials:</p>
-                    <FontAwesomeIcon icon={faLinkedin} size="lg" />
-                    <FontAwesomeIcon icon={faSquareGithub} size="lg" />
+                    <a href="https://www.linkedin.com/in/olga-veikova-230a8b316/" target="_blank">
+                        <FontAwesomeIcon
+                            icon={faLinkedin}
+                            size="lg"
+                            aria-label="link to LinkedIn page"
+                        />
+                    </a>
+
+                    <a href="https://github.com/snezhny-mishka" target="_blank">
+                        <FontAwesomeIcon
+                            icon={faSquareGithub}
+                            size="lg"
+                            aria-label="link to GitHub page"
+                        />
+                    </a>
                 </div>
                 <div className="flex flex-col justify-center items-center xs:col-span-2 md:col-span-1 text-xs">
                     <p>Build with React + Vite</p>
