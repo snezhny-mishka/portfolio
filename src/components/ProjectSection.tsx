2abaa13926eed167c4571e0957029690c6fd0a9d
unknown <xopox2015@gmail.com>
2025-11-11 11:05:30 +0100

    feat: add responsiveness

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -38,13 +38,13 @@ export const ProjectSection =
             </div>
 
             <div className="relative col-span-6">
-                <div className="absolute inset-0 bg-yellow opacity-60 m-auto rounded-xl md:max-w-3/4"></div>
-                <img src={imgSrc} alt={alt} className="m-auto rounded-xl md:max-w-3/4" />
+                <div className="absolute inset-0 bg-yellow opacity-60 m-auto rounded-xl md:max-w-3/4 lg:h-100"></div>
+                <img src={imgSrc} alt={alt} className="m-auto rounded-xl md:max-w-3/4 lg:h-100" />
                 <div className="lg:top-8 lg:right-10 absolute inset-0 lg:inset-auto bg-grey active:opacity-0 m-auto lg:m-0 p-3 rounded-xl lg:w-100 max-w-5/6 md:max-w-2/3 h-fit transition duration-300 ease-in-out delay-150">
                     <p className="px-4 py-2 lg:text-md text-xs sm:text-base">{description}</p>
                 </div>
             </div>
-            <div className="relative col-span-2 col-start-2">
+            <div className="relative col-span-2 lg:col-span-1 col-start-2 lg:col-start-3">
                 <div className="top-2 -left-10 -z-10 absolute bg-yellow rounded-full w-18 h-18"></div>
                 <div className="col-span-2 col-start-2 px-2 py-1 border-1 rounded-xl w-fit">
                     <p className="text-sm">built with</p>
