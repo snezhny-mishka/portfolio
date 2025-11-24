c6146db1cc44e16315cd9816194021efb7ba8bc4
unknown <xopox2015@gmail.com>
2025-11-24 14:50:25 +0100

    style: add photo animation and  responsiveness

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -9,18 +9,18 @@ export const AboutSection = ({ ref, techStack }: AboutProps) => {
     return (
         <>
             <section ref={ref} className="mt-6 mb-6">
-                <h1 className="mb-6 ml-4 md:ml-15 heading-section">ABOUT ME</h1>
-                <div className="flex md:flex-row-reverse flex-col">
-                    <div className="relative h-fit">
+                <h1 className="mb-6 ml-6 md:ml-14 heading-section">ABOUT ME</h1>
+                <div className="flex md:flex-row-reverse flex-col md:gap-8 md:px-25">
+                    <div className="relative self-center max-w-2/4 md:max-w-2/5 xl:max-w-1/5">
                         <img
-                            src="./src/assets/my-portrait.png"
+                            src="./src/assets/portrait.JPG"
                             alt="my photo"
-                            className="peer m-auto rounded-2xl max-w-2/3 lg:max-w-2/4 transition-all translate-x-0 translate-y-0 hover:-translate-x-3 hover:-translate-y-3 duration-300 ease-in-out delay-150"
+                            className="peer rounded-2xl transition-all translate-x-0 translate-y-0 hover:-translate-x-3 hover:-translate-y-3 duration-300 ease-in-out delay-150"
                         />
-                        <div className="absolute inset-0 bg-yellow opacity-45 peer-hover:opacity-0 m-auto rounded-2xl max-w-2/3 lg:max-w-2/4 transition peer-hover:-translate-x-3 peer-hover:-translate-y-3 duration-300 ease-in-out delay-150 pointer-events-none"></div>
-                        <div className="-z-20 absolute inset-0 bg-yellow m-auto rounded-2xl max-w-2/3 lg:max-w-2/4 transition-all translate-x-0 translate-y-0 peer-hover:translate-x-3 peer-hover:translate-y-3 duration-300 ease-in-out delay-150"></div>
+                        <div className="absolute inset-0 bg-yellow opacity-45 peer-hover:opacity-0 rounded-2xl transition peer-hover:-translate-x-3 peer-hover:-translate-y-3 duration-300 ease-in-out delay-150 pointer-events-none"></div>
+                        <div className="-z-20 absolute inset-0 bg-yellow rounded-2xl transition-all translate-x-0 translate-y-0 peer-hover:translate-x-3 peer-hover:translate-y-3 duration-300 ease-in-out delay-150"></div>
                     </div>
-                    <article className="m-auto md:ml-20 pt-5 max-w-3/4 text-sm sm:text-base">
+                    <article className="self-center md:ml-6 pt-5 max-w-3/4 text-sm sm:text-base">
                         I started out as an illustrator, and my curiosity about how things work
                         online led me into web development. I bring a creative eye to building
                         projects, and each one of them is a chance to explore, experiment, and grow
