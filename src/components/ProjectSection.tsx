7482fac8f9e71908321cbd74ef71fdf3942299aa
unknown <xopox2015@gmail.com>
2025-11-25 15:34:35 +0100

    feat: add project website link to description for smaller screens

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -45,7 +45,10 @@ export const ProjectSection = ({
                 ></a>
                 <img src={imgSrc} alt={alt} className="m-auto rounded-xl md:max-w-3/4 lg:h-100" />
                 <div className="lg:top-8 lg:right-10 absolute inset-0 lg:inset-auto bg-grey active:opacity-0 m-auto lg:m-0 p-3 rounded-xl lg:w-100 max-w-5/6 md:max-w-2/3 h-fit transition duration-300 ease-in-out delay-150">
-                    <p className="px-4 py-2 lg:text-md text-xs sm:text-base">{description}</p>
+                    <a href={webPage} target="_blank" aria-label="See project webpage" className="md:hidden block">
+                        <p className="px-4 py-2 lg:text-md text-xs sm:text-base">{description}</p>
+                    </a>
+                    <p className="hidden md:block px-4 py-2 lg:text-md text-xs sm:text-base">{description}</p>
                 </div>
             </div>
             <div className="relative col-span-2 lg:col-span-1 col-start-2 lg:col-start-3">
@@ -65,7 +68,7 @@ export const ProjectSection = ({
                 <a
                     href={gitPage}
                     target="_blank"
-                    className="hover:text-yellow"
+                    className="hover:text-yellow active:text-yellow"
                     aria-label="Project GitHub page"
                 >
                     <FontAwesomeIcon icon={faSquareGithub} size="2x" />
@@ -73,7 +76,7 @@ export const ProjectSection = ({
                 <a
                     href={webPage}
                     target="_blank"
-                    className="hover:text-yellow"
+                    className="hover:text-yellow active:text-yellow"
                     aria-label="Project website"
                 >
                     <FontAwesomeIcon icon={faSquareArrowUpRight} size="2x" />
